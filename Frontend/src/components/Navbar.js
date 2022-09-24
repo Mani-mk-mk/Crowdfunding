731b541c7dbae69829a0982512bd51efa18285d5
Manigandan Kasimani <manigandankasimani@gmail.com>
2022-09-25 01:16:35 +0530

    Add Connect Metamask

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useMetamask from "../hooks/useMetamask";
 import "./css/Navbar.css";
 
 function Navbar() {
-  const [isSignedIn, setisSignedIn] = useState(true);
+	const [isSignedIn, setisSignedIn] = useState(true);
 
-  return (
-    <nav className="navbar">
-      <Link style={{ textDecoration: "none" }} to="/">
-        <h1 className="nav-title">Sasta Kickstarter</h1>
-      </Link>
-      {!isSignedIn ? (
-        <ul className="nav-text-right">
-          <li>
-            <button className="purple-btn">Sign in</button>
-          </li>
-        </ul>
-      ) : (
-        <ul className="nav-text-right">
-          <li>
-            <Link style={{ textDecoration: "none" }} to="/browse">
-              <h4>Browse</h4>
-            </Link>
-          </li>
-          <li>
-            <Link style={{ textDecoration: "none" }} to="/myprojects">
-              <h4>My Projects</h4>
-            </Link>
-          </li>
-        </ul>
-      )}
-    </nav>
-  );
+	const { connect, disconnect, account } = useMetamask();
+
+	return (
+		<nav className="navbar">
+			<Link style={{ textDecoration: "none" }} to="/">
+				<h1 className="nav-title">Sasta Kickstarter</h1>
+			</Link>
+			{!isSignedIn ? (
+				<ul className="nav-text-right">
+					<li>
+						<button onClick={connect} className="purple-btn">
+							Sign in
+						</button>
+					</li>
+				</ul>
+			) : (
+				// For testing purpose
+				// <ul className="nav-text-right">
+				// 	<li>
+				// 		<button onClick={connect} className="purple-btn">
+				// 			Sign in
+				// 		</button>
+				// 	</li>
+				//  </ul>
+
+				<ul className="nav-text-right">
+					<li>
+						<Link style={{ textDecoration: "none" }} to="/browse">
+							<h4>Browse</h4>
+						</Link>
+					</li>
+					<li>
+						<Link style={{ textDecoration: "none" }} to="/myprojects">
+							<h4>My Projects</h4>
+						</Link>
+					</li>
+				</ul>
+			)}
+		</nav>
+	);
 }
 
 export default Navbar;
