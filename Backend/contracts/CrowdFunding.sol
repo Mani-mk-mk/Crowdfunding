c778b2b6c64967b685eb9947434bd0749c143cd1
Manigandan Kasimani <72310576+Mani-mk-mk@users.noreply.github.com>
2022-09-24 16:58:28 +0530

    Add Project Description and End Date
    Updated CrowdFunding.sol by adding the project Description and time period for the camapign

--- Backend/contracts/CrowdFunding.sol
+++ Backend/contracts/CrowdFunding.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-/// Need a better name ;)
-contract Parent {
+///@title CrowdFunding
+///@author Mani, Prithvi, Asswin
+///@notice Parent Deployer which will deploy contract for each and every campaign
+contract Deployer {
   
-  address[] public deployedProjects;
+  CrowdFunding[] public deployedProjects;
 
-  function createFunding(uint256 _amount)  public {
-    address deployedAddress = new CrowdFunding(_amount, msg.sender);
+  function createFunding(uint256 _amount, string memory _name, string memory _description, uint256 _endDate, uint256 _requiredFund)  public {
+    CrowdFunding deployedAddress = new CrowdFunding(_amount, msg.sender, _name, _description, _endDate, _requiredFund);
     deployedProjects.push(deployedAddress);
   }
 
-  function getDeployedProjects() public view returns (address[]) {
+  function getDeployedProjects() public view returns (CrowdFunding[] memory) {
     return deployedProjects;
   }
 
@@ -21,64 +23,89 @@ contract Parent {
 
 }
 
+///@title CrowdFunding
+///@author Mani, Prithvi, Asswin
+///@notice A contract will be deployed for each of the campaign
 contract CrowdFunding {
 
-  struct Requests {
+  struct Request {
     string requestDescription;
     uint256 amount;
-    address receipent;
+    address payable receipent;
     bool complete;
     uint256 approvalCount;
     mapping (address => bool) approvals;
   }
 
+  uint numRequests;
+  mapping (uint => Request) requests;
+
+  //Project Descriptions
+  string  public name;
+  string  public description;
+  uint256 public startDate;
+  uint256 public endDate;
+  uint256 public requiredFund;
+
   address public admin;
-  uint256 public contribution;  
-  uint256 public contributorsCount;
+  uint256 public minimumContribution;  
+  uint256 public totalContributors;
 
-  // address[] public contributors;
+  mapping (address => bool) public contributors;
 
-  mapping (address => bool) public contibutors;
-
-  Requests[] public requests;
-
-  constructor(uint256 _contribution, address _admin) public {
+  constructor(uint256 _minimumContribution, address _admin, string memory _name, string memory _description, uint256 _duration, uint256 _requiredFund) public {
     //msg.sender => basically the address of the creator of the contract
+    name = _name;
     admin = _admin;
-    contribution = _contribution;
-    contributorsCount = 0;
+    description = _description;
+    //Using the 
+    startDate = block.timestamp;
+    endDate = (_duration * 1 days) + startDate;
+    requiredFund = _requiredFund;
+    minimumContribution = _minimumContribution;
+    totalContributors = 0;
   }
 
   function contribute() public payable {
+    require(block.timestamp < endDate, "The duration for funding this project is over");
     //msg.value => amount in wei sent by a address
-    require(msg.value >= contribution, "Please send minimum amount of ethers");
+    require(msg.value >= minimumContribution, "Please send minimum amount of ethers");
     contributors[msg.sender] = true;
-    contributorsCount++;
+    totalContributors++;
   }
 
 
-  function createRequest(string _requestDescription, uint256 _amount, address _receipent ) public {
+  // Returns the amount of money collected for the funding
+  function getAmountReceived() public view returns (uint256) {
+    return address(this).balance;
+  }
+
+
+  function createRequest(string memory _requestDescription, uint256 _amount, address _receipent ) public {
     //Since this is an owner only function no other person should be able to access it
     require(msg.sender == admin, "This is an owner only function");
-    Requests memory _request = Requests({
-      requestDescription: _requestDescription,
-      amount: _amount,
-      receipent: _receipent,
-      complete: false,
-      approvalCount: 0
-    });
-
-    requests.push(_request);
+    Request storage _request = requests[numRequests++];
+    _request.requestDescription = _requestDescription;
+    _request.amount = _amount;
+    _request.receipent = payable(_receipent);
+    _request.complete = false;
+    _request.approvalCount = 0;
   }
 
-    //************************************************** */
-    ///@title: approveRequest
-    ///@desc: Function to approve the request created by the admin;
-  function approveRequest(uint index) public {
+  
+  // Calculates the time remaining in UNIX standard
+  function timeRemainig() public view returns(uint256) {
+    require(block.timestamp <= endDate, "The duration for funding this project is over");
+    return endDate - block.timestamp;
+  }
 
-    Requests storage _request = requests[index];
+  //************************************************** */
+  //title: approveRequest
+  // Function to approve the request created by the admin;
+  function approveRequest(uint index) public {
+    Request storage _request = requests[index];
     require(contributors[msg.sender], "You are not a contributor");
-    require(!_request.aprovals[msg.sender], "You've already voted on this request");
+    // require(!_request.aprovals[msg.sender], "You've already voted on this request");
 
     _request.approvalCount++;
     _request.approvals[msg.sender] = true;
@@ -86,16 +113,14 @@ contract CrowdFunding {
 
   //************************************************** */
   /// Function to finalize the transaction and send the money to Vendors;
-  function sendTransaction(uint index)  public  () {
-    Requests _request = requests[index];
+  function sendTransaction(uint index) public {
+    Request storage _request = requests[index];
 
     require(msg.sender == admin, "This is an owner only function");
     require(!_request.complete, "The request has been finalized already");
-    require(approvalCount > (contributorsCount/2), "You need more votes buddy!!");
-
+    require(_request.approvalCount > (totalContributors/2), "You need more votes buddy!!");
     //Sending the amount to the receipent
-    _request.receipent.transfer(_request.amount)
-
+    _request.receipent.transfer(_request.amount);
     _request.complete = true;
   }
  
