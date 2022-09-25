82adfacfd7300849ecbf8c2eaae8069c7190d03c
Manigandan Kasimani <manigandankasimani@gmail.com>
2022-09-25 12:35:14 +0530

    Write interact scripts

--- Frontend/Backend/api/interact.js
+++ Frontend/Backend/api/interact.js
@@ -5,6 +5,7 @@ const ethers = require("ethers");
 const config = require("../config");
 
 const contract = require("../artifacts/contracts/CrowdFunding.sol/Deployer.json");
+const crowdFundingContract = require("../artifacts/contracts/CrowdFunding.sol/CrowdFunding.json");
 
 const deployer = new web3.eth.Contract(contract.abi, config.contractAddress);
 
@@ -37,7 +38,7 @@ const createFunding = async (
 			requiredAmount
 		)
 		.send({
-			from: "0x199a87570C0C202fe5D703423491B188552d7868",
+			from: address,
 			gas: 3000000,
 		});
 
@@ -46,13 +47,150 @@ const createFunding = async (
 
 getDeployedProjects();
 getDeployedProjectsLength();
-createFunding(
-	"0x199a87570C0C202fe5D703423491B188552d7868",
-	"Test",
-	"This is a test to check whether this transaction takes place properly",
-	ethers.utils.parseEther("0.1"),
-	4,
-	ethers.utils.parseEther("5")
-);
+// createFunding(
+// 	"0x199a87570C0C202fe5D703423491B188552d7868",
+// 	"Test",
+// 	"This is a test to check whether this transaction takes place properly",
+// 	ethers.utils.parseEther("0.1"),
+// 	4,
+// 	ethers.utils.parseEther("5")
+// );
 
 //Create interact script for CrowdFunding Contract now
+
+const contribute = async (index, amount, address) => {
+	try {
+		const contractAddress = (await getDeployedProjects())[index];
+		const crowdFunding = new web3.eth.Contract(
+			crowdFundingContract.abi,
+			contractAddress
+		);
+		await crowdFunding.methods.contribute().send({
+			from: address,
+			value: amount,
+			gas: 3000000,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	return true;
+};
+
+const isContributor = async (index, address) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	const isContributor = await crowdFunding.methods.contributors(address).call();
+	return isContributor;
+};
+
+const getOwner = async (index) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	const owner = await crowdFunding.methods.admin().call();
+	return owner;
+};
+
+const getBalanceOf = async (index) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	const balance = await crowdFunding.methods.getAmountReceived().call();
+	console.log(balance);
+	return balance;
+};
+
+const createRequest = async (
+	index,
+	description,
+	amount,
+	receipent,
+	address
+) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	try {
+		await crowdFunding.methods
+			.createRequest(description, amount, receipent)
+			.send({
+				from: address,
+				gas: 3000000,
+			});
+	} catch (error) {
+		console.error(
+			"This is a owner only function -> only owner of a campaign can create a request"
+		);
+		console.log(error);
+	}
+	return true;
+};
+
+const timeRemaining = async (index) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	const time = await crowdFunding.methods.timeRemaining().call();
+	console.log(time);
+	return time;
+};
+
+const approveRequest = async (index, requestIndex, address) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	try {
+		await crowdFunding.methods.approveRequest(requestIndex).send({
+			from: address,
+			gas: 3000000,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	return true;
+};
+
+const finalizeRequest = async (index, requestIndex, address) => {
+	const contractAddress = (await getDeployedProjects())[index];
+	const crowdFunding = new web3.eth.Contract(
+		crowdFundingContract.abi,
+		contractAddress
+	);
+
+	try {
+		await crowdFunding.methods.sendTransaction(requestIndex).send({
+			from: address,
+			gas: 3000000,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	return true;
+};
+
+// contribute(
+// 	0,
+// 	ethers.utils.parseEther("1"),
+// 	"0x7Ee0e98D96876839525e8C678c8823aFB2D900d3"
+// );
+
+// getBalanceOf(0);
--- Frontend/Backend/test/CrowdFunding.test.js
+++ Frontend/Backend/test/CrowdFunding.test.js
@@ -1,55 +1,95 @@
+/* eslint-disable jest/valid-expect */
+
 const { expect } = require("chai");
+// const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
-describe("CrowdFunding Contract", () => {
-	let owners;
-	let CrowdFunding;
-	let crowdFunding;
-	beforeEach(async () => {
-		owners = (await ethers.getSigners())[0];
-		// console.log(owners);
-		CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-	});
+const Deployer = hre.ethers.getContractFactory("Deployer");
+const CrowdFunding = hre.ethers.getContractFactory("CrowdFunding");
 
-	it("Deploying the Contract", async () => {
-		crowdFunding = await CrowdFunding.deploy(
-			1000000,
-			owners,
+let owner, add1, add2;
+let deployer;
+let crowdFunding;
+let crowdFundingAddress;
+
+beforeEach(async () => {
+	[owner, add1, add2] = await ethers.getSigners();
+	// console.log(owner);
+	deployer = await Deployer.deploy();
+	// console.log(deployer);
+
+	await deployer.methods
+		.createFunding(
+			ethers.utils.parseEther("0.1"),
 			"Test",
-			"Test",
-			45,
-			2000000000000000000
-		);
-		console.log(`The contract is deployed at ${crowdFunding.address}`);
-		expect(crowdFunding.address).to.exist;
-	});
+			"This is a test to check whether this transaction takes place properly",
+			4,
+			ethers.utils.parseEther("5")
+		)
+		.send({ from: owner, gas: 3000000 });
 
-	it("Correct owner", async () => {
-		const owner = await crowdFunding.admin();
-		// console.log(owner);
-		expect(owner).to.equals(owners.address);
-	});
+	[crowdFundingAddress] = await deployer.methods.getDeployedProjects().call();
 
-	//****************************************** */
-	//** Contributing some amount of ether */
+	crowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+	crowdFunding = await crowdFunding.attach(crowdFundingAddress);
+});
 
-	it("Contributing to funding", async () => {
-		// const contribute = await crowdFunding.contribute();
-		const signers = await ethers.getSigners();
-		const signer = await ethers.getSigner(signers[1].address);
-		const txnHash = await signer.sendTransaction({
-			to: crowdFunding.address,
-			value: ethers.utils.parseEther("1"),
-		});
-
-		console.log(txnHash);
-		expect(txnHash).to.exist;
-	});
-
-	it("Checking the Balance", async () => {
-		const balance = await crowdFunding.getAmountReceived();
-		console.log(balance);
-		expect(balance).toString.equals(ethers.utils.parseEther("1"));
+describe("CrowdFunding", () => {
+	it("marks caller as campaign manager", async () => {
+		const manager = await crowdFunding.methods.owner.call();
+		expect(manager).to.equals(owner.address);
 	});
 });
+
+// describe("CrowdFunding Contract", () => {
+// 	let owners;
+// 	let CrowdFunding;
+// 	let crowdFunding;
+// 	beforeEach(async () => {
+// 		owners = (await ethers.getSigners())[0];
+// 		// console.log(owners);
+// 		CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+// 	});
+
+// 	it("Deploying the Contract", async () => {
+// 		crowdFunding = await CrowdFunding.deploy(
+// 			1000000,
+// 			owners,
+// 			"Test",
+// 			"Test",
+// 			45,
+// 			2000000000000000000
+// 		);
+// 		console.log(`The contract is deployed at ${crowdFunding.address}`);
+// 		expect(crowdFunding.address).to.exist;
+// 	});
+
+// 	it("Correct owner", async () => {
+// 		const owner = await crowdFunding.admin();
+// 		// console.log(owner);
+// 		expect(owner).to.equals(owners.address);
+// 	});
+
+// 	//****************************************** */
+// 	//** Contributing some amount of ether */
+
+// 	it("Contributing to funding", async () => {
+// 		// const contribute = await crowdFunding.contribute();
+// 		const signers = await ethers.getSigners();
+// 		const signer = await ethers.getSigner(signers[1].address);
+// 		const txnHash = await signer.sendTransaction({
+// 			to: crowdFunding.address,
+// 			value: ethers.utils.parseEther("1"),
+// 		});
+
+// 		console.log(txnHash);
+// 		expect(txnHash).to.exist;
+// 	});
+
+// 	it("Checking the Balance", async () => {
+// 		const balance = await crowdFunding.getAmountReceived();
+// 		console.log(balance);
+// 		expect(balance).toString.equals(ethers.utils.parseEther("1"));
+// 	});
+// });
